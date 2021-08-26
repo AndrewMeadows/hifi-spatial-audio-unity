24e50f9bd7a0b6bac80ee4e6822179896bc09ae6
Andrew Meadows <andrew@highfidelity.io>
2021-08-26 14:43:00 -0700

    remove debug cruft

--- Samples~/Bumpers/Scripts/MyFollower.cs
+++ Samples~/Bumpers/Scripts/MyFollower.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MyFollower : MonoBehaviour {
     public MyBumper target;
-    //int foo = 0;
 
     MyBumper _target;
 
@@ -12,19 +11,6 @@ public class MyFollower : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        /*
-        ++foo;
-        if (0 == (foo % 50)) {
-            Vector3 cpos = transform.position;
-            Vector3 tpos = Vector3.zero;
-            if (_target != null) {
-                tpos = _target.transform.position;
-            }
-            string msg = string.Format("adebug camera=<{0},{1},{2}> target=<{3},{4},{5}>",
-                    cpos.x, cpos.y, cpos.z, tpos.x, tpos.y, tpos.z);
-            Debug.Log(msg);
-        }
-        */
     }
 
     public void SetTarget(MyBumper myBumper) {
